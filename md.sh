fa29b41da32a7610a319492d0ad068aad5d20e19
silentdan44 <142716048+silentdan44@users.noreply.github.com>
2023-09-28 00:49:38 +0300

    md.sh

--- md.sh
+++ md.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#SBATCH -p hpc4-el7-gpu-3d
+#SBATCH -p hpc5-el7-gpu-3d
 #SBATCH -J gromacs              
 #SBATCH -o grom.%j.out       
 #SBATCH -e grom.%j.err        
 #SBATCH -N 1                  
-#SBATCH -n 6               
+#SBATCH -n 1               
 #SBATCH --exclusive
-#SBATCH --gres=gpu:6
+#SBATCH --gres=gpu:1
 
 module purge
 module load gromacs/2021
 
-mpirun -n 6 gmx_mpi mdrun -deffnm md_0_1 -nb gpu
+mpirun -n 1 gmx_mpi mdrun -rdd 2.1 -ntomp 1 -deffnm md_0_1 -nb gpu
+
